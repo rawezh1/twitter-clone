47576cba9933016175a7e022a96e62977d277e71
Rawezh <rawezhmarif@gmail.com>
2021-07-19 17:26:02 +0300

    finish left side bar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Account from './components/account';
 import TweetObj from './components/create-tweet-obj';
 import DefaultPic from './components/images/default-pic.png'
 import DefualtBanner from './components/images/default-header.jpg'
+import MainFeed from './components/main-feed';
 import './App.css';
 
 function App() {
   const testAccount = new AccountObj('Test',DefaultPic,DefualtBanner);
   const testTweet = new TweetObj(testAccount,'Hello World!',[], new Date());
   testAccount.listoftweets.push(testTweet);
+  const listOfAccount = [testAccount, testAccount, testAccount, testAccount, testAccount, testAccount, testAccount, testAccount, testAccount, testAccount];
+
   return (
     <div className="App">
-      <Account info = {testAccount}/>
+      <MainFeed accounts = {listOfAccount}/>
     </div>
   );
 }
---
+++ src/components/main-feed.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Tweet from './tweet';
+import './account.css';
+import LeftBar from './left-panel';
+
+function MainFeed(props) {
+  const makeFeed = (accounts) => {
+    const feed = accounts.map ((account => {
+        return account.listoftweets.map((tweet) => {
+            return <Tweet content={tweet} />;
+          });
+    }))
+    console.log(feed);
+    return feed;
+  };
+  return (
+    <div className='account'>
+      <LeftBar />
+      <div>
+        <div>{makeFeed(props.accounts)}</div>
+      </div>
+      <div ></div>
+    </div>
+  );
+}
+export default MainFeed;
